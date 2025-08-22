cfd01be7d2b355373bb231291014691f1292a636
Gokuldev23 <gkforgeneral@gmail.com>
2025-08-22 16:28:52 +0530

    Fix : Broken initial Loading spinner fixed

--- src/components/customUi/loadingSpinner/CustomLoading.tsx
+++ src/components/customUi/loadingSpinner/CustomLoading.tsx
@@ -2,13 +2,15 @@ import './loading.css'
 
 export default function CustomLoading() {
   return (
-    <div className="loader">
+    <div className='loading-container'>
+      <div className="loader">
         <div className="loader__bar"></div>
         <div className="loader__bar"></div>
         <div className="loader__bar"></div>
         <div className="loader__bar"></div>
         <div className="loader__bar"></div>
         <div className="loader__ball"></div>
+      </div>
     </div>
   )
 }
